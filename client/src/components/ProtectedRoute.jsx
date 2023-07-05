413e3acf63e1e5d3436e00a286de95f85b3a9b3f
Kyle Shutt <xavior04350@yahoo.com>
2023-07-05 11:39:40 -0500

    FE updates

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import { Navigate, redirect } from 'react-router-dom'
 
 function ProtectedRoute(props){
     const { token, children, redirectTo } = props
-    return token ? children : <Navigate to={redirectTo}  />
+    return token ? children : <Navigate to={redirectTo}   />
 }
 
 export default ProtectedRoute
