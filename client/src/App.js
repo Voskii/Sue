55a46c8759353b19bf69ad8dac5a68676cedaba2
Kyle Shutt <xavior04350@yahoo.com>
2023-07-05 11:38:33 -0500

    front end updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
-
+import React, {useContext} from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import ProtectedRoute from './components/ProtectedRoute.jsx'
 import './style.css';
 
 function App() {
+  const { token, logout } = useContext(UserContext)
   return (
     <div className="App">
       Hello Sue!
---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Navigate, redirect } from 'react-router-dom'
+
+function ProtectedRoute(props){
+    const { token, children, redirectTo } = props
+    return token ? children : <Navigate to={redirectTo} />
+}
+
+export default ProtectedRoute
