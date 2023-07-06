c879568c542c6c93e62743c3ae9233ce468c5083
Kyle Shutt <xavior04350@yahoo.com>
2023-07-06 18:07:42 -0500

    login functioning

--- client/src/components/Hey.jsx
+++ client/src/components/Hey.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { UserContext } from '../context/UserProvider.jsx'
 
 export default function Hey(){
+    const { user } = useContext(UserContext)
     return (
-        <div>
-            :)
+        <div className='heyContainer'>
+            
         </div>
     )
 }
